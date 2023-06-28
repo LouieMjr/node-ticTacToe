a6cb642401ae965fcca2d67f6a15564b66ffae7b
Louie <lmendezjr024@gmail.com>
2023-06-27 22:21:14 -0400

    Shortend checkWin function for rows and columns.. Still need to shorten the Diagonal win logic

--- index.js
+++ index.js
@@ -54,37 +54,58 @@ const validBoardSpace = (letter) => {
 
 // CHECK FOR A TRIO OF X's OR O's /// CHECK IF ANY SPACE ON BOARD IS LEFT
 const checkWin = () => {
-  // Rows loopd
-  for (let x = 0; x < 3; x++) {
-    // CHECK ROWS FOR WIN
-    if (board[x][0] === 'X' && board[x][1] === 'X' && board[x][2] === 'X') return `Player-X wins on X axis ${printBoard()}`
-
-    if (board[x][0] === 'O' && board[x][1] === 'O' && board[x][2] === 'O') return `Player-O wins on X axis ${printBoard()}`
-    
-    // Columns loop
-    for (let y = 0; y < 3; y++) {
-      // CHECK COLUMNS FOR WIN
-      if (board[0][y] === 'X' && board[1][y] === 'X' && board[2][y] === 'X') return `Player-X wins on Y axis ${printBoard()}`
-
-      if (board[0][y] === 'O' && board[1][y] === 'O' && board[2][y] === 'O') return `Player-O wins on Y axis ${printBoard()}`
-      
-      
-      // CHECK DIAGONALLY FROM TOP LEFT TO BOTTOM RIGHT
-      if (board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X') return `Player-X wins on Diagonally ${printBoard()}`
-
-      if (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O') return `Player-O wins on Diagonally ${printBoard()}`
-      
-      // CHECK DIAGONALLY FROM TOP RIGHT to BOTTOM LEFT
-      if (board[0][2] === 'X' && board[1][1] === 'X' && board[2][0] === 'X') return `Player-X wins on Diagonally ${printBoard()}`
-      
-      if (board[0][2] === 'O' && board[1][1] === 'O' && board[2][0] === 'O') return `Player-O wins on Diagonally ${printBoard()}`
-      
-      // console.log(`checking for open space on board ${board[x][y]}` )
-      if (board[x][y] === '-') return playGame();
+  const size = board.length;
+  let rowWin = false
+  let colWin = false
+  
+  for (let x = 0; x < size; x++) {
+    if (
+      (board[x][0] !== '-' && board[x][0] === board[x][1] && board[x][0] === board[x][2]) ||
+      (board[0][x] !== '-' && board[0][x] === board[1][x] && board[0][x] === board[2][x])
+    ) {
+      rowWin = true
+      colWin = true
     }
+    if(rowWin && board[x][0] !== '-') return `Player ${board[x][0]} Wins on Row ${x}! ${printBoard()} `;
+    if(colWin && board[0][x] !== '-') return `Player ${board[0][x]} Wins on Column ${x}! ${printBoard()} `;
   }
-  console.log(printBoard());
-  return 'NO SPACES LEFT, TIE GAME';
+
+
+  return playGame();
+  
+  
+  
+  // // Rows loop
+  // for (let x = 0; x < 3; x++) {
+  //   // CHECK ROWS FOR WIN
+  //   if (board[x][0] === 'X' && board[x][1] === 'X' && board[x][2] === 'X') return `Player-X wins on X axis ${printBoard()}`
+
+  //   if (board[x][0] === 'O' && board[x][1] === 'O' && board[x][2] === 'O') return `Player-O wins on X axis ${printBoard()}`
+    
+  //   // Columns loop
+  //   for (let y = 0; y < 3; y++) {
+  //     // CHECK COLUMNS FOR WIN
+  //     if (board[0][y] === 'X' && board[1][y] === 'X' && board[2][y] === 'X') return `Player-X wins on Y axis ${printBoard()}`
+
+  //     if (board[0][y] === 'O' && board[1][y] === 'O' && board[2][y] === 'O') return `Player-O wins on Y axis ${printBoard()}`
+      
+      
+  //     // CHECK DIAGONALLY FROM TOP LEFT TO BOTTOM RIGHT
+  //     if (board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X') return `Player-X wins on Diagonally ${printBoard()}`
+
+  //     if (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O') return `Player-O wins on Diagonally ${printBoard()}`
+      
+  //     // CHECK DIAGONALLY FROM TOP RIGHT to BOTTOM LEFT
+  //     if (board[0][2] === 'X' && board[1][1] === 'X' && board[2][0] === 'X') return `Player-X wins on Diagonally ${printBoard()}`
+      
+  //     if (board[0][2] === 'O' && board[1][1] === 'O' && board[2][0] === 'O') return `Player-O wins on Diagonally ${printBoard()}`
+      
+  //     // console.log(`checking for open space on board ${board[x][y]}` )
+  //     if (board[x][y] === '-') return playGame();
+  //   }
+  // }
+  // console.log(printBoard());
+  // return 'NO SPACES LEFT, TIE GAME';
 }
 
 const printBoard = () => {
